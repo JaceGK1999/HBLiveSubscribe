d8ad040a19fa14726963acfaad4f9a0f8c91ffbc
Jacegk1999 <Jacegk1999@gmail.com>
2022-05-24 00:03:43 -0700

    fixed naming error in services

--- src/services/messages.js
+++ src/services/messages.js
@@ -15,8 +15,8 @@ export async function sendMessage(email, status = 0) {
 }
 
 export function subscribe(onMessage = (_message) => {}) {
-  const onMessage = client.from('messages').on('INSERT').subscribe();
-  return parseData(onMessage);
+  const subMessage = client.from('messages').on('INSERT').subscribe();
+  return parseData(subMessage);
   // TODO: Subscribe to changes for the `messages` table
   // and call `onMessage` with the newly added row
 }
